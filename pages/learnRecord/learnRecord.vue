7c9bcaa99e4f28cec549774f7146e71f4ffdf676
adam_sgl <1024844072@qq.com>
2019-12-05 23:00:05 +0800

    移动jssdk到components

--- pages/learnRecord/learnRecord.vue
+++ pages/learnRecord/learnRecord.vue
@@ -20,7 +20,7 @@
 </template>
 
 <script>
-	import uCharts from '@/js_sdk/u-charts/u-charts/u-charts.js';
+    import uCharts from "@/components/u-charts/u-charts.js"
 	var _self;
 	var canvaLineA=null;
 	export default {
